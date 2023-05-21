654f1ce032a7c824cb11a4c5bbf47d6018c36ea6
korchasa <korchasa@icloud.com>
2023-05-21 15:10:35 +0300

    feat: proper character/prompt separation

--- pkg/character/prompt/prompt.go
+++ pkg/character/prompt/prompt.go
@@ -3,19 +3,16 @@ package prompt
 import (
     "fmt"
     "github.com/Masterminds/sprig/v3"
-    "github.com/korchasa/spilka/pkg/actions"
     "os"
     "strings"
     "text/template"
 )
 
-const templatePath = "./pkg/character/prompt/character.toml.gotpl"
-
 type Generator struct {
     tpl *template.Template
 }
 
-func NewGenerator() (*Generator, error) {
+func NewGenerator(templatePath string) (*Generator, error) {
     cnt, err := os.ReadFile(templatePath)
     if err != nil {
         return nil, fmt.Errorf("failed to read template: %v", err)
@@ -29,31 +26,11 @@ func NewGenerator() (*Generator, error) {
     }, nil
 }
 
-func (g *Generator) GeneratePrompt(problem string, char *CharacterSpec, team []*CharacterSpec, history []actions.Action) (string, error) {
-    view := View{
-        Problem:        problem,
-        Character:      char,
-        TeamCharacters: team,
-        History:        history,
-    }
+func (g *Generator) Prompt(variables interface{}) (string, error) {
     stringWriter := &strings.Builder{}
-    err := g.tpl.Execute(stringWriter, view)
+    err := g.tpl.Execute(stringWriter, variables)
     if err != nil {
         return "", fmt.Errorf("failed to execute template: %v", err)
     }
     return stringWriter.String(), nil
-}
-
-type View struct {
-    Problem        string
-    Character      *CharacterSpec
-    TeamCharacters []*CharacterSpec
-    History        []actions.Action
-}
-
-type CharacterSpec struct {
-    Name        string
-    Role        string
-    Description string
-    Commands    []actions.Command
 }
